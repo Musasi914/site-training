e532c4b5a3986a765ddb442f03d4ce43877e23bb
Hidetaka Matsuda <sopmod120@gmail.com>
2025-10-22 20:41:11 +0900

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Site1 from "./sites/site1.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Site1 />
+  </StrictMode>
+);
---
+++ src/sites/site1.tsx
@@ -0,0 +1,7 @@
+export default function Site1() {
+  return (
+    <>
+      <h1 className="font-bold underline">hello</h1>
+    </>
+  );
+}
